be96ac6c38bb44b80d1a3655a75f48fe06a63df5
边雷 <775884216@qq.com>
2016-11-30 19:18:15 +0800

    定位大头针反地理编码, 显示定位点title和subTitle

--- MapDemo/MapDemo/Map/BLMapViewController.m
+++ MapDemo/MapDemo/Map/BLMapViewController.m
@@ -8,8 +8,9 @@
 
 #import "BLMapViewController.h"
 #import <MapKit/MapKit.h>
-@interface BLMapViewController ()
+@interface BLMapViewController ()<MKMapViewDelegate>
 @property(nonatomic, weak) MKMapView *map;
+@property(nonatomic, strong) CLLocationManager *mgr;
 @end
 
 @implementation BLMapViewController
@@ -28,6 +29,35 @@
     MKMapView *map = [[MKMapView alloc]initWithFrame:CGRectMake(0, 64, self.view.bounds.size.width, self.view.bounds.size.height - 64)];
     [self.view addSubview:map];
     self.map = map;
+    
+    //授权   设置info.plist
+    self.mgr = [CLLocationManager new];
+    if ([self.mgr respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [self.mgr requestWhenInUseAuthorization];
+    }
+    self.map.userTrackingMode = MKUserTrackingModeFollow;
+    
+    self.map.delegate = self;
 }
+
+#pragma mark - 定位大头针 反地理编码
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    CLGeocoder *gecoder = [CLGeocoder new];
+    
+    CLLocation *location = userLocation.location;
+    [gecoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        //判断地标对象
+        if (placemarks.count == 0 || error) {
+            return ;
+        }
+        
+        //设置数据
+        self.map.userLocation.title = placemarks.lastObject.locality;
+        self.map.userLocation.subtitle = placemarks.lastObject.name;
+    }];
+}
+
 
 @end
