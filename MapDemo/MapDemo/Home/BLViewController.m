ee36034620a1bad8288b57aa4ae2847d07226cb7
边雷 <775884216@qq.com>
2016-11-30 17:00:42 +0800

    前期准备

---
+++ MapDemo/MapDemo/Home/BLViewController.m
@@ -0,0 +1,22 @@
+//
+//  BLViewController.m
+//  MapDemo
+//
+//  Created by 边雷 on 16/11/30.
+//  Copyright © 2016年 Mac-b. All rights reserved.
+//
+
+#import "BLViewController.h"
+
+@interface BLViewController ()
+
+@end
+
+@implementation BLViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor magentaColor];
+}
+
+@end
